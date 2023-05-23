901d2d0d5158f48cf9cf3876feedcb034377fd9c
Azamat <amas9nine@mail.ru>
2023-05-23 21:43:45 +0600

    first commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+
+    // comments
+
+    /*
+     * comments
+     * comments
+     */
+    
+    public static void main(String[] args) {
+        System.out.println("hellфo \\!!!\n\n\t hellфo!!\n\n \"hellф!\" ");
+        System.out.println("hellфo!!");
+        System.out.println("hellф!");
+    }
+}
